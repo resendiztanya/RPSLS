3998b7a079e5bb373bd2240799ee33f0dba2e522
Tanya <tanyaerese@yahoo.com>
2023-10-10 18:50:46 -0500

    update remote repository

--- RPSLS/Game.cs
+++ RPSLS/Game.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace RPSLS
@@ -46,7 +48,44 @@ namespace RPSLS
 
         public void RunGame()
         {
+            //Step 1: Introduce RPSLS Game. Explain and list rules of the game. 
             WelcomeMessage();
-        }
+            Console.WriteLine("-Rock crushes Scissors -Scissors cuts paper -Paper cover Rock " +
+                "-Rock crushes Lizard -Lizard poisons spock -Spock smashes scissors " +
+                "-Scissors decapitates Lizard -Lizard eats Paper -Paper disproves Spock " +
+                "-Spock vaporizes Rock");
+
+            Console.WriteLine("The winner of 2 out of 3 rounds will be declared the final " +
+                "winner between 2 players.");
+
+            //Step 2: Ask if there will be 1 or 2 human players participating in the game 
+            int singlePlayer = 1;
+            int numberofPlayers = 2;
+            Console.WriteLine("How many human players will be playing, 1 or 2?");
+            string numberofHumanPlayersInput = "1";
+
+            int val = 0;
+            Int32.TryParse(numberofHumanPlayersInput, out val);
+
+            numberofHumanPlayersInput = Console.ReadLine();
+
+            if (val == numberofPlayers)
+            {
+                
+                Console.WriteLine("Player 1 vs. Player 2 will now commence, enter Player 1 name");
+                string playerOneName = Console.ReadLine();
+            }
+            else if (val == singlePlayer)
+            {
+                Console.WriteLine("Player 1 vs. Computer will now commence.");
+            }
+            //Step 3: 1st round will begin and a gesture will be decided upon by each player
+            
+
+                //Step 4: "winner" or "tie" and score will be displayed at the end of each round
+                //step 5: Final score will be displayed after 2 rounds have been won by a player 
+                //step 6: Game will reset 
+
+            }
     }
 }
--- RPSLS/Player.cs
+++ RPSLS/Player.cs
@@ -1,30 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace RPSLS
 {
-    internal abstract class Player
+    internal abstract class Player 
     {
         //Member Variabes (HAS A)
         public string name;
         public List<string> gestures;
         public string chosenGesture;
         public int score;
+    
 
         //Constructor
         public Player(string name)
         {
             this.name = name;
-            gestures = new List<string> {"rock", "paper", "scissors", "lizard", "Spock" };
+            //gestures = new List<string> {"rock", "paper", "scissors", "lizard", "Spock" };
             chosenGesture = "";
             score = 0;
-        }
 
+            Player HumanPlayer;
+             
+              
+
+            List<string> gestures = new List<string>
+            {
+                "Rock",
+                "Paper",
+                "Scissors",
+                "Lizard",
+                "Spock",
+            };
+
+            Console.WriteLine(" Player 1: Choose a gesture.");
+            string firstGesture = Console.ReadLine();
+
+            string output = string.Join(Environment.NewLine, gestures.ToArray());
+            if (firstGesture != output)
+            {
+                Console.WriteLine("That gesture is invalid");
+            }
+            
+            }
+        
         //Member Methods (CAN DO)
         //This abstract method must be overridden by the child Player classes
         public abstract void ChooseGesture();
+
+        
+
+        
+
+        
     }
+    
 }
